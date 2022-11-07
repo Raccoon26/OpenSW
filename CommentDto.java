c11fdfb88a6cd4e13530ec09caa5432ca04617bf
Raccoon <33raccoon33@gmail.com>
2022-11-07 13:16:54 +0900

    댓글기능_완성

--- CommentDto.java
+++ CommentDto.java
@@ -39,3 +39,30 @@ public class CommentDto {
             return comments;
         }
     }
+    /**
+     * 댓글 정보를 리턴할 응답(Response) 클래스
+     * Entity 클래스를 생성자 파라미터로 받아 데이터를 Dto로 변환하여 응답
+     * 별도의 전달 객체를 활용해 연관관계를 맺은 엔티티간의 무한참조를 방지
+     */
+    @RequiredArgsConstructor
+    @Getter
+    public static class Response {
+        private Long id;
+        private String comment;
+        private String createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
+        private String modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
+        private String nickname;
+        private Long userId;
+        private Long postsId;
+        /* Entity -> Dto*/
+        public Response(Comment comment) {
+            this.id = comment.getId();
+            this.comment = comment.getComment();
+            this.createdDate = comment.getCreatedDate();
+            this.modifiedDate = comment.getModifiedDate();
+            this.nickname = comment.getUser().getNickname();
+            this.userId = comment.getUser().getId();
+            this.postsId = comment.getPosts().getId();
+        }
+    }
+}
